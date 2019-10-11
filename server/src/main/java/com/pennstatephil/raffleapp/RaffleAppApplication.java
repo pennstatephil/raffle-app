da8000c84a41297d985c76e081b1e9a5cb3622e8
Phil Hurwitz <pennstatephil@gmail.com>
2019-10-10 22:50:26 -0600

    starter files from start.spring.io

---
+++ server/src/main/java/com/pennstatephil/raffleapp/RaffleAppApplication.java
@@ -0,0 +1,13 @@
+package com.pennstatephil.raffleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/pennstatephil/raffleapp/RaffleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pennstatephil.raffleapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaffleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
